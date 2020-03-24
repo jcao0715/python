9a5dad956a52576662a02bb5fd8b009bef38d824
Jing Cao <jcao0715@gmail.com>
2020-03-24 16:06:14 -0400

    4. more control flow tools

--- controlFlow.py
+++ controlFlow.py
@@ -49,10 +49,24 @@ for num in range(2, 10):
 # pass - does nothing
 # used when statement is requred syntactically but requires no action
 
-#defining functions
+# defining functions
+# basically a method in java
 def fib(n):
 	a, b = 0, 1
 	while a < n:
 		print(a, end=' ')
 		a, b = b, a + b
 	print()
+
+f = fib #renamed function fib to f
+
+def fib2(n):
+	result = []
+	a, b = 0, 1
+	while a < n:
+		result.append(a)
+		a, b = b, a + b
+	return result
+
+f100 = fib2(100)
+print(f100)
