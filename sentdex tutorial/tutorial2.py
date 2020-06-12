b93c3e62d5ef1c226e31ec457f3f34d3189afd53
Jing Cao <jcao0715@gmail.com>
2020-06-12 01:11:23 -0400

    tuples, strings, for loops

---
+++ sentdex tutorial/tutorial2.py
@@ -0,0 +1,9 @@
+programming_languages = "Python", "Java", "C++", "C#"
+#can be put in parantheses and type doesn't change
+
+print(type(programming_languages)) #object's type
+
+for language in programming_languages:
+    print(language)
+
+#tuples are immutable, lists are not
